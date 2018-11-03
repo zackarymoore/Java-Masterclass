cf9a671f1a9ca7e55ba4851129c32d57b4f41419
zackarymoore <42760286+zackarymoore@users.noreply.github.com>
2018-11-02 23:24:14 -0400

    Section 3, Lecture 16. Challenge Commit.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Zack!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myAnswer = 1000 - myTotal;
+
+        System.out.println(myAnswer);
     }
 }
