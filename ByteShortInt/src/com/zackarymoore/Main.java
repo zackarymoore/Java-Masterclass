6019e96b7cdb9b62df51bb388b31ba28028ffe77
zackarymoore <42760286+zackarymoore@users.noreply.github.com>
2018-11-03 00:08:47 -0400

    Section 3, Lecture 17. Challenge commit.

--- ByteShortInt/src/com/zackarymoore/Main.java
+++ ByteShortInt/src/com/zackarymoore/Main.java
@@ -3,24 +3,12 @@ package com.zackarymoore;
 public class Main {
 
     public static void main(String[] args) {
-        // int has a width of 32
-        int myMinValue = -2_147_483_648;
-        int myMaxValue = 2_147_483_647;
-        int myTotal = (myMinValue/2);
-        System.out.println("myTotal = " + myTotal);
-
-        // byte has a width of 8
-        byte myByteValue = -128;
-        byte myNewByteValue = (byte) (myByteValue/2);
-        System.out.println("myNewByteValue = " + myNewByteValue);
-
-        // short has a width of 16
-        short myShortValue = -32768;
-        short myNewShortValue = (short) (myShortValue/2);
-        System.out.println("myShortValue = " + myNewShortValue);
-
-        // long has a width of 64
-        long myLongValue = 9_223_372_036_854_775_807L;
-        System.out.println("myLongValue = " + myLongValue);
+        byte myByteValue = 10;
+        short myShortValue = 20;
+        int myIntValue = 50;
+        long longTotal = 50_000L + 10L * (myByteValue + myShortValue + myIntValue);
+        short shortTotal = (short) (1000 + 10 * (myByteValue + myShortValue + myIntValue));
+        System.out.println("longTotal = " + longTotal);
+        System.out.println("shortTotal = " + shortTotal);
     }
 }
