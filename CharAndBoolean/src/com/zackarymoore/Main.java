e18baea350a834ad1e1d1e79e19118f0e60844ec
zackarymoore <42760286+zackarymoore@users.noreply.github.com>
2018-11-03 00:39:15 -0400

    Section 3, Lecture 19. Initial commit.

---
+++ CharAndBoolean/src/com/zackarymoore/Main.java
@@ -0,0 +1,12 @@
+package com.zackarymoore;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+    }
+}
