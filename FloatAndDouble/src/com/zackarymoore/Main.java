c6dcf5f8267f34a411b3ae3642a70391dce3da27
zackarymoore <42760286+zackarymoore@users.noreply.github.com>
2018-11-03 00:32:30 -0400

    Section 3, Lecture 18. Challenge commit.

--- FloatAndDouble/src/com/zackarymoore/Main.java
+++ FloatAndDouble/src/com/zackarymoore/Main.java
@@ -3,15 +3,9 @@ package com.zackarymoore;
 public class Main {
 
     public static void main(String[] args) {
-        // width of int = 32 (4 bytes).
-        int myIntValue = 5 / 3;
-        // width of float = 32 (4 bytes).
-        float myFloatValue = 5f / 3f;
-        // width of double = 64 (8 bytes).
-        double myDoubleValue = 5d / 3d;
+        double numPounds = 200d;
+        double convertedKilograms = numPounds * 0.45359237d;
 
-        System.out.println("myIntValue = " + myIntValue);
-        System.out.println("myFloatValue = " + myFloatValue);
-        System.out.println("myDoubleValue = " + myDoubleValue);
+        System.out.println("Kilograms = " + convertedKilograms);
     }
 }
