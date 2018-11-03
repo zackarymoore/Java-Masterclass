e82b1d5623f142dd0e3ea2768302630f7bdc2af7
zackarymoore <42760286+zackarymoore@users.noreply.github.com>
2018-11-03 13:47:25 -0400

    Section 3, Lecture 22. Initial commit.

--- Operators/src/com/zackarymoore/Main.java
+++ Operators/src/com/zackarymoore/Main.java
@@ -51,8 +51,25 @@ public class Main {
         if (topScore < 100)
             System.out.println("You got the high score!");
 
-        int secondTopScore = 81;
-        if(topScore > secondTopScore && topScore < 100)
+        int secondTopScore = 95;
+        if((topScore > secondTopScore) && (topScore < 100))
             System.out.println("Greater than second top score and less than 100");
+
+        if ((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of theses tests is true");
+
+        int newValue = 50;
+        if (newValue == 50)
+            System.out.println("This is true");
+
+        boolean isCar = true ;
+        if (isCar)
+            System.out.println("This is not supposed to happen");
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar)
+            System.out.println("wasCar is true");
+
     }
 }
